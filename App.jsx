9a3facf9d89b3ac1e7f85f30ddd0044346340ab4
azra uçar <azra.ucar@ogr.gelisim.edu.tr>
2026-04-07 12:20:01 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function SifreUretici(){
+  const[uzunluk,setUzunluk]=useState(10);
+  const[sifre,setSifre]=useState("");
+
+  const uret=()=>{
+    const torba="AaBbCcÇçDdEeFfGgĞğHhIıİiJjKkLlMmNnOoÖöPpRrSsŞşTtUuÜüVvYyZz0123456789";
+    let sonuc="";
+    for(let i=0; i < uzunluk; i++){
+      const rastgeleYer= Math.floor(Math.random() * torba.length);
+      sonuc += torba.charAt(rastgeleYer);
+    }
+    setSifre(sonuc);
+  };
+  return(
+    <div className='sifre-kart'>
+      <h1>ŞİFRE ÜRETİCİ</h1>
+      <div className='sifre-ekrani'>
+        {sifre || "****"}
+      </div>
+
+      <div style={{marginBottom:"20px"}}>
+        <input
+         type='number'
+         value={uzunluk}
+        onChange={(e) => setUzunluk(e.target.value)}/>
+        <span>Karakter</span>
+      </div>
+      <button onClick={uret}>Yeni Şifre Üret</button>
+    </div>
+  );
+}
+export default SifreUretici;
